54278ac9c18ab0f72f68a4a433ef9046d454bfe6
Raju <31988870+pvjraju96@users.noreply.github.com>
2022-10-19 00:24:00 +0530

    Committing Exercises 4 and 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -10,12 +10,20 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(
+                e -> System.out.println(e),
+                err -> System.out.println(err.getMessage()),
+                () -> System.out.println("Mono completed"));
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        Integer number = ReactiveSources.intNumberMono().block();
+        System.out.println("number : " + number);
 
-        System.out.println("Press a key to end");
-        System.in.read();
+        User user = ReactiveSources.userMono().block();
+        System.out.println("User :: " + user);
+        ///System.out.println("Press a key to end");
+        ///System.in.read();
     }
 
 }
